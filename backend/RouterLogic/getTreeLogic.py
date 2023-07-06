0a71fa39ce1b7cdfd2d9a343cc81fb1606e77b30
Jay <jaykumar20march@gmail.com>
2023-07-06 23:30:44 +0530

    add: added parse_data() in getTreeLogic.py
    - added parse_data() function which successfully extracts nodes and edges from the dependency tree.
    
    Signed-off-by: 35C4n0r <jaykumar20march@gmail.com>

--- backend/RouterLogic/getTreeLogic.py
+++ backend/RouterLogic/getTreeLogic.py
@@ -11,6 +11,7 @@ async def getTreeLogic(file: UploadFile):
     unprocessed_deps_tree = get_pipgrip_output(lines)
     edges = []
     nodes = {}
+    parse_data(tree=unprocessed_deps_tree, nodes=nodes, edges=edges, depth=0, node_id=0)
     return {
         "success": True,
         "nodes": unprocessed_deps_tree,
@@ -25,3 +26,26 @@ def get_pipgrip_output(lines):
         arg.append(line)
     result = subprocess.run(arg, stdout=subprocess.PIPE)
     return result.stdout.decode('utf-8')
+
+
+def parse_data(tree, nodes, edges, depth, node_id, parent=None):
+    for req in tree:
+        node_name = f"{req['name']}-{req['version']}"
+        if node_name not in nodes:
+            if parent is not None:
+                nodes[node_name] = [node_id, depth + 1]
+            else:
+                nodes[node_name] = [node_id, depth]
+            node_id += 1
+        parse_data(tree=req.get('dependencies', []), nodes=nodes, edges=edges, parent=nodes[node_name], node_id=node_id,
+                   depth=depth + 1)
+        if parent is not None:
+            edges.append({
+                'from': parent[0],
+                'to': nodes[node_name][0]
+            })
+        else:
+            edges.append({
+                'from': 0,
+                'to': nodes[node_name][0]
+            })
